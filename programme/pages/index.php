d8d7d286a1c99e7920987ba165703cea6270de21
Akeops <lalanne.andoni1@gmail.com>
2023-11-05 21:27:54 +0100

    Ajout du dossier source avec le dossier images et mon image d'exemple

--- programme/pages/index.php
+++ programme/pages/index.php
@@ -6,13 +6,22 @@ require '../utile/formatage.php';
 <div class="container text-center" id="accueil">
     <?= formatageTitre("Andoni Lalanne-Berdouticq") ?>
     <div class="row">
-    <div class="col">
-      Column
+        <div class="col">
+            <div class="col-12 col-md-6 col-lg-4">
+                <img src="../source/images/exemple.png" class="card-img-top" height="165px" alt="...">
+            </div>
+        </div>
+        <div class="col">
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card" style="width: auto;">
+                    <div class="card-body">
+                        <h5 class="card-title">Portfolio</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col">
-      Column
-    </div>
-  </div>
 </div>
 
 <div class="text-center mt-3">
