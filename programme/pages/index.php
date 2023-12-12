decb306aab71f2715dbcb3d75381037a16569bd2
Akeops <lalanne.andoni1@gmail.com>
2023-12-12 22:05:14 +0100

    J'ai amélioré le texte de ma présentation

--- programme/pages/index.php
+++ programme/pages/index.php
@@ -12,7 +12,7 @@ require '../utile/formatage.php';
                 <img src="../source/images/photoPresentation.png" class="card-img-top img-fluid" alt="photo de moi">
             </div>
             <div class="col-12 col-xl-6 presentationDescription shadow-lg text-start">
-                    <p>Bonjour, je m'appelle Andoni Lalanne-Berdouticq, un passionné de développement informatique avec un BTS en SIO option SLAM. Ayant récemment repris mes études dans la lignée de mon BTS à Efrei, je me spécialise actuellement dans le développement Full-Stack en Javascript. Mon parcours m'a doté de solides compétences en PHP, Bootstrap, CSS, Docker et Javascript. </p>
+                    <p>Bonjour, je suis Andoni Lalanne-Berdouticq, passionné de développement informatique et titulaire d'un BTS en SIO avec une spécialisation en SLAM. Ayant récemment repris mes études à l'Efrei, je me concentre actuellement sur le développement Full-Stack. Mon parcours m'a conféré des compétences robustes en PHP, Bootstrap, CSS et Javascript. À ce stade, je suis particulièrement enthousiaste à l'idée de me former sur une stack polyvalente, me permettant d'exceller aussi bien dans le frontend que dans le backend du développement informatique.</p>
             </div>
         </div>
         <div class="col-12 text-center boutonCV">
